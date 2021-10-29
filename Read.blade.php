e751b1989a5d94ef028306552cfab5679df0d242
OkabeEruna <196015@std.hi-joho.ac.jp>
2021-10-29 13:39:35 +0900

    html追加

---
+++ Read.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="{{  asset('css/read.css') }}" />
+  <title>読み取り画面</title>
+</head>
+<body>
+<h2>バーコードリーダで<br>バーコードを読み取ってください</h1>
+<div class="img">
+<img src="{{ asset('img/QR.png') }}" class="img-1">
+</div>
+<form action="{{action('App\Http\Controllers\TableController@move')}}" method="POST">
+            @csrf
+             <input type="submit" name="submit" value="->" />
+        </form>
+
+
+</body>
+</html>
---
+++ check.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="{{  asset('css/table.css') }}" />
+        <title>読み取り画面</title>
+    </head>
+    <h2>情報出力</h2>
+    <body>
+        <br> 
+    <table >
+        <tr>
+            <th>入庫ID</th>
+            <th>ワクチンID</th>
+            <th>注射器ID</th>
+            <th>数量/本</th>
+            <th>製造元</th>
+            <th>有効期限</th>  
+        </tr>
+        <tr>
+            <td>・・・・</td>
+            <td>・・・・</td>
+            <td>・・・・</td>
+            <td >・・</td>
+            <td  >・・・・</td>
+            <td >・・/・・/・・</td>
+        </tr>
+    </table>
+<br>
+
+
+<div class='frame'>
+<input type="submit" name="submit" value="戻る" class="custom-btn btn-1"/>
+<input type="submit" name="submit" value="取消" class="custom-btn btn-2"/>
+<input type="submit" name="submit" value="登録" class="custom-btn btn-4"/>
+</div>
+
+
+
+        
+    <body>
+  
+</html>    
