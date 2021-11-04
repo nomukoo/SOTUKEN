45bcc7f13045a03e8d7c248a8a30d6f0791e3e83
Harimoto70 <196001@std.hi-joho.ac.jp>
2021-11-04 12:01:50 +0900

    viewのリネームと整理

--- nyuko/views/error/warehousing_error.blade.php
+++ nyuko/views/error/warehousing_error.blade.php
@@ -2,13 +2,13 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>編集完了画面</title>
+  <title>入庫エラー画面</title>
 </head>
 <body>
-<h1>編集が完了しました。</h1>
+<h1>エラーになりました</h1>
 <form  action="{{action('vaccineTableController@vaccineOut')}}" method="POST">
   @csrf
-  <input type="submit" name="back" value="戻る">
+  <input type="submit" name="back" value="入庫受付">
 </form>
 </body>
 </html>
