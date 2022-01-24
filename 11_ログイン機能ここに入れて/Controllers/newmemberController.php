e8c4a37b8e9ba4becbcb9eefbfc14f06e18d84c4
gar-g <77521225+gar-g@users.noreply.github.com>
2022-01-24 10:36:03 +0900

    ログイン機能修正

--- 11_ログイン機能ここに入れて/Controllers/newmemberController.php
+++ 11_ログイン機能ここに入れて/Controllers/newmemberController.php
@@ -11,16 +11,9 @@ class newmemberController extends Controller
 	public function move(Request $request)
 	{
 		
-
-		$request->session()->put('userID',$request->input('userID'));
-		$request->session()->put('password',$request->input('password'));
-		$request->session()->put('email',$request->input('email'));
-
 		$booking = new Patient();
-		$booking->patient_ID = $request->session()->get('userID');
-		$booking->patient_password = $request->session()->get('password');
-		$booking->patient_email = $request->session()->get('email');
-
+		$booking->patient_ID = $request->input('userID');
+		$booking->patient_password = $request->input('password');
 		$booking->save();
 
 		return view('newmember');
