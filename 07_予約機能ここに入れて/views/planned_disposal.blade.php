0975047f620c664eb6d288158e07bb5b9be146d4
12yasuda <196002@std.hi-joho.ac.jp>
2022-01-26 17:21:18 +0900

    予約機能の変更

---
+++ 07_予約機能ここに入れて/views/planned_disposal.blade.php
@@ -0,0 +1,235 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.79.0">
+    <title>廃棄予定一覧画面</title>
+
+    <link rel="canonical" href="https://getbootstrap.jp/docs/5.0/examples/dashboard/">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <!-- Bootstrap core CSS -->
+<link href=https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.js" integrity="sha256-tcqPYPyxU+Fsv5sVdvnxLYJ7Jq9wWpi4twZbtZ0ubY8=" crossorigin="anonymous"></script>
+<script>
+        $(function(){
+        $(".btn-dell").click(function(){
+                if(confirm("本当に削除しますか？")){
+                }else{
+                    return false;
+                }
+            });
+        });
+    </script>
+
+<meta name="theme-color" content="#7952b3">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="{{  asset('css/dashboard.css') }}" />
+    <link href="/css/dilution.css" rel="stylesheet">
+
+
+  </head>
+  <body>
+  <?php 
+  $emp_info = session()->get('emp_Info');
+  $json_emp_info = json_encode($emp_info);
+?>
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand1 col-md-3 col-lg-2 me-0 px-3 a1">ワクチン在庫管理システム</a>
+  <ul class="nav pull-right">
+  <li class="nav-item">
+  <a class="nav-link text-white"href="#">{{'ログイン中: '}}{{$emp_info['staff_name']}}</a>
+  </li>
+  <li class="nav-item">
+      <a class="nav-link text-white" href="#">
+      <form action="{{url('/')}}" method="get"  class="form">
+                     @csrf
+            <input type="submit" name="submit" value="ホーム" class="btn1" />
+      </form>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-white"href="{{ url('/emp_logout')}}">サインアウト</a>
+    </li>
+  </ul>
+    </header>
+
+
+    <div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+        <h6 class="text-primary" style="padding:5px;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+                メニュー
+              </h6>
+<li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">入庫</a>
+          <span data-feather="shopping-cart"></span>
+            <div class="dropdown-menu" aria-labelledby="dropdown03">
+                <a class="dropdown-item" id="itm" href="/barcoderead">ワクチン入庫</a>
+                <a class="dropdown-item" id="itm" href="/barcode_read_syringe">注射器入庫</a>
+            </div>
+          </li>
+<li class="nav-item">
+<a class="nav-link" href="/defrost_read">
+  <span data-feather="shopping-cart"></span>
+  出庫登録
+</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="shipping_list">
+  <span data-feather="users"></span>
+  出庫予定閲覧
+</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="/dilution_read">
+  <span data-feather="users"></span>
+  希釈登録
+</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="/PlannedDisposal">
+  <span data-feather="users"></span>
+  廃棄登録
+</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="/">
+  <span data-feather="users"></span>
+  在庫一覧
+</a>
+</li>
+@if (false)
+<li class="nav-item">
+<a class="nav-link" href="hospitalinput">
+  <span data-feather="users"></span>
+  従業員登録
+</a>
+</li>
+@endif    
+      </div>
+    </nav>
+
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h1">廃棄予定一覧</h1>
+        <div id="smartwizard" class="sw-theme-arrows">
+        <ul class="nav nav-tabs step-anchor">
+        <li class="active"><a>廃棄予定<br><small></small></a></li>
+        </ul>
+      </div>
+
+
+
+        <div class="btn-toolbar mb-2 mb-md-0">
+
+        </div>
+    </div>
+
+    <div class="m-5">
+    </div>
+
+    <div class="text-center">
+    <div class="text-danger">
+    <h2 color="red">廃棄予定</h2>
+    </div>
+    </div>
+@if(!$DB_PlannedDisposal_get->isEmpty())
+
+    <div class="m-5">
+    <div class="text-center">
+    <table class="table table-bordered border-dark" >
+
+    <tbody>
+    <thead>
+        <tr>
+            <th>ロット番号</th>
+            <th>有効期限</th>
+            <th></th>
+        </tr>
+        </thead>
+     @foreach ($DB_PlannedDisposal_get as $list)
+
+            <tr>
+
+
+
+                <td>{{ $list ->  lot_number}}</td>
+                <td>{{ $list -> expair }}</td>
+                <td>
+                    <form action="/Disposal/{{$list->PlannedDisposal_ID}}" method="POST">
+                        @csrf
+                    <input type="submit" value="廃棄" class="btn btn-danger btn-sm btn-dell">
+                    </form>
+                </td>
+
+            </tr>
+            @endforeach
+    </tbody>
+</table>
+ @else
+        <h3>廃棄予定のワクチンはありません</h3>
+   @endif
+</div>
+
+
+<div class="m-5">
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="{{ asset('/js/dilution.js') }}"></script>
+    <script src="{{ asset('/js/dilution_confirm.js') }}"></script>
+
+
+  </body>
+</html>
